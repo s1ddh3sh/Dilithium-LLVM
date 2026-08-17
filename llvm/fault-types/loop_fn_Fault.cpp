0338f45482e72203207d0241c37e1803491c1f6c
siddhesh <talktosiddhesh.shinde@gmail.com>
2026-08-17 14:05:18 +0530

    faulttypes

--- llvm/fault-types/loop_fn_Fault.cpp
+++ llvm/fault-types/loop_fn_Fault.cpp
@@ -293,9 +293,9 @@ public:
       PN->addIncoming(incomingVal, prevIterExit);
     }
 
-    // for (BasicBlock *BB : origBlocks) {
-    //   BB->eraseFromParent();
-    // }
+    for (BasicBlock *BB : origBlocks) {
+      BB->eraseFromParent();
+    }
   }
 };
 
@@ -970,9 +970,9 @@ public:
       PN->addIncoming(incomingVal, prevIterExit);
     }
 
-    // for (BasicBlock *BB : origBlocks) {
-    //   BB->eraseFromParent();
-    // }
+    for (BasicBlock *BB : origBlocks) {
+      BB->eraseFromParent();
+    }
   }
 };
 
@@ -1006,9 +1006,9 @@ Function *getOrCloneHelper(Module &M, Module &SourceM, StringRef Name) {
 
 void replaceMemoryIntrinsics(Module &M, Module &SourceM) {
 
-  Function *MayoMemset = getOrCloneHelper(M, SourceM, "kyber_memset");
+  Function *MayoMemset = getOrCloneHelper(M, SourceM, "dilithium_memset");
 
-  Function *MayoMemcpy = getOrCloneHelper(M, SourceM, "kyber_memcpy");
+  Function *MayoMemcpy = getOrCloneHelper(M, SourceM, "dilithium_memcpy");
 
   SmallVector<CallInst *, 64> Worklist;
 
